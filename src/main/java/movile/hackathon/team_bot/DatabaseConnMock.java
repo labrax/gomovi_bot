4172ff189a0687eeb1617c39291c43cda017108c
Victor R. Cardoso <victor.roth.cardoso@gmail.com>
2016-03-19 18:17:55 -0300

    resultados busca textual & etc

--- src/main/java/movile/hackathon/team_bot/DatabaseConnMock.java
+++ src/main/java/movile/hackathon/team_bot/DatabaseConnMock.java
@@ -84,6 +84,14 @@ public class DatabaseConnMock {
 		return null;
 	}
 	
+	public String getResultadosBuscaLocalizacaoTextual(String sub_categoria, String localizacao) {
+		return "";
+	}
+	
+	public String getResultadosBuscaTexto(String texto) {
+		return "";
+	}
+	
 	/**
 	 * Retorna os serviçoes listados pelo usuário
 	 * @param user
