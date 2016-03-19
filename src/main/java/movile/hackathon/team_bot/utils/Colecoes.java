5002df69f518706e55b6a32215ec761aa0cdf948
Alvaro Silva <alvaro.silva@sofist.com.br>
2016-03-19 14:50:49 -0300

    remocao de zueira no arquivo

--- src/main/java/movile/hackathon/team_bot/utils/Colecoes.java
+++ src/main/java/movile/hackathon/team_bot/utils/Colecoes.java
@@ -1,12 +1,6 @@
 package movile.hackathon.team_bot.utils;
 
-/**
- * Created with IntelliJ IDEA.
- * User: zanoni
- * Date: 5/19/14
- * Time: 10:38 AM
- * To change this template use File | Settings | File Templates.
- */
+
 public enum Colecoes {
     DADOS_USUARIOS,
 }
